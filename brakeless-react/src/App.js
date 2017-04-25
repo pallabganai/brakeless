0e7746990066bec4a73c968e5046f5fb852f09a2
pallab.ganai <p.k.ganai@gmail.com>
2017-04-25 17:53:28 +1000

    lambda added

--- brakeless-react/src/App.js
+++ brakeless-react/src/App.js
@@ -3,7 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+      super(props, context);
+      this.state = {
+        items:[]
+      }
+  }
   render() {
+    const {items} = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,8 +21,49 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        { items.map(item => <p>{item.id}</p>)}
       </div>
     );
+  }
+
+  componentDidMount() {
+    var that= this;
+    console.log("In componentDidMount");
+
+    var apigClient = window.apigClientFactory.newClient();
+
+    var params = {
+      // This is where any modeled request parameters should be added.
+      // The key is the parameter name, as it is defined in the API in API Gateway.
+      // param0: '',
+      // param1: ''
+    };
+
+    var body = {
+      // This is where you define the body of the request,
+    };
+
+    var additionalParams = {
+      // If there are any unmodeled query parameters or headers that must be
+      //   sent with the request, add them here.
+      headers: {
+        // param0: '',
+        // param1: ''
+      },
+      queryParams: {
+        // param0: '',
+        // param1: ''
+      }
+    };
+
+    apigClient.breaklessGet(params, body, additionalParams)
+        .then(function(result){
+          console.log(result.data);
+          that.setState(result.data);
+        }).catch( function(result){
+          console.log(result);
+        });
   }
 }
 
